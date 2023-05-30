000b3c72314977ad4246347bc0d3463987a95a6e
OsMax <www.osmax@gmail.com>
2023-05-30 15:44:14 +0300

    hw-07 is ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const place = document.querySelector(".gallery");
+for (let item of galleryItems) {
+  const newLi = document.createElement("li");
+  newLi.classList.add("gallery__item");
+  let str = `<a class="gallery__link" href="${item.original}" onclick="return false">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>`;
+  newLi.insertAdjacentHTML("beforeend", str);
+  place.appendChild(newLi);
+}
 
-console.log(galleryItems);
+place.addEventListener("click", (e) => {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">`
+  );
+  // document.body.style.overflow = "hidden";
+  instance.show();
+
+  const closeModal = (e) => {
+    if (e.code !== "Space") {
+      return;
+    }
+    // document.body.style.overflow = "scroll";
+    instance.close(() => {
+      document.removeEventListener("keydown", closeModal);
+      // console.log("!!!");
+    });
+  };
+
+  document.addEventListener("keydown", closeModal);
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const place = document.querySelector(".gallery");
 console.log(galleryItems);
+for (let item of galleryItems) {
+  const newLi = document.createElement("li");
+  newLi.classList.add("gallery__item");
+  let str = `<a class="gallery__link" href="${item.original}" onclick="return false">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>`;
+  newLi.insertAdjacentHTML("beforeend", str);
+  place.appendChild(newLi);
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  caption: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox", function () {
+  // do something…
+});
